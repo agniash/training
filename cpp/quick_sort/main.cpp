4b692de99c390b6801a3bcb30c0fd05e7f13ebf5
agniash <agniash.development@gmail.com>
2017-10-24 17:26:49 +0300

    iterative quick sort

--- cpp/quick_sort/main.cpp
+++ cpp/quick_sort/main.cpp
@@ -12,11 +12,14 @@ void test(size_t size)
     std::iota(v1.begin(), v1.end(), 0);
     std::random_shuffle(v1.begin(), v1.end());
     auto v2 = v1;
+    auto v3 = v1;
 
     std::sort(v1.begin(), v1.end());
     QuickSort(v2.begin(), v2.end());
+    IterativeQuickSort(v3.begin(), v3.end());
 
     ASSERT_EQ(v1, v2);
+    ASSERT_EQ(v1, v3);
 }
 
 TEST(QuickSortTest, TestZero)
--- cpp/quick_sort/quick_sort.h
+++ cpp/quick_sort/quick_sort.h
@@ -7,6 +7,8 @@
 #include <functional>
 #include <utility>
 #include <cstdlib>
+#include <stack>
+#include <tuple>
 
 namespace NQuickSortTraits {
     const size_t QuadraticSortCount = 7;
@@ -33,6 +35,37 @@ void QuickSort(Iterator first, Iterator last, const Comparator& comparator = Com
         } else {
             QuickSort(middle, last);
             last = middle;
+        }
+    }
+}
+
+template <class Iterator, class Comparator = std::less<decltype(*std::declval<Iterator>())>>
+void IterativeQuickSort(Iterator first, Iterator last, const Comparator& comparator = Comparator())
+{
+    std::stack<std::pair<Iterator, Iterator>> stack;
+    stack.push({first, last});
+
+    while (!stack.empty()) {
+        auto [left, right] = stack.top();
+        stack.pop();
+
+        auto count = std::distance(left, right);
+        if (count < NQuickSortTraits::QuadraticSortCount) {
+            InsertionSort(left, right, comparator);
+            continue;
+        }
+
+        auto pivot = *std::next(left, std::rand() % count);
+        auto predicate = [&](auto x) {
+            return comparator(x, pivot);
+        };
+        Iterator middle = Partition(left, right, predicate);
+        if (std::distance(left, middle) < std::distance(middle, right)) {
+            stack.push({middle, right});
+            stack.push({left, middle});
+        } else {
+            stack.push({left, middle});
+            stack.push({middle, right});
         }
     }
 }
