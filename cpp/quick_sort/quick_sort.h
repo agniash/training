9f88d9d2510381a80b4127aebf1a5ab91fdc841e
agniash <agniash.development@gmail.com>
2017-10-24 15:00:35 +0300

    tail recursion modification

--- cpp/quick_sort/quick_sort.h
+++ cpp/quick_sort/quick_sort.h
@@ -15,17 +15,24 @@ namespace NQuickSortTraits {
 template <class Iterator, class Comparator = std::less<decltype(*std::declval<Iterator>())>>
 void QuickSort(Iterator first, Iterator last, const Comparator& comparator = Comparator())
 {
-    auto count = std::distance(first, last);
-    if (count < NQuickSortTraits::QuadraticSortCount) {
-        InsertionSort(first, last, comparator);
-        return;
-    }
+    while (true) {
+        auto count = std::distance(first, last);
+        if (count < NQuickSortTraits::QuadraticSortCount) {
+            InsertionSort(first, last, comparator);
+            return;
+        }
 
-    auto pivot = *std::next(first, std::rand() % count);
-    auto predicate = [&](auto x) {
-        return comparator(x, pivot);
-    };
-    Iterator middle = Partition(first, last, predicate);
-    QuickSort(first, middle);
-    QuickSort(middle, last);
+        auto pivot = *std::next(first, std::rand() % count);
+        auto predicate = [&](auto x) {
+            return comparator(x, pivot);
+        };
+        Iterator middle = Partition(first, last, predicate);
+        if (std::distance(first, middle) < std::distance(middle, last)) {
+            QuickSort(first, middle);
+            first = middle;
+        } else {
+            QuickSort(middle, last);
+            last = middle;
+        }
+    }
 }
