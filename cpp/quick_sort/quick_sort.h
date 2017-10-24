f07e5307ed597ca672b887cb3b222a68a678fc2f
agniash <agniash.development@gmail.com>
2017-10-24 10:47:18 +0300

    quadratic sort improvement

--- cpp/quick_sort/quick_sort.h
+++ cpp/quick_sort/quick_sort.h
@@ -1,17 +1,23 @@
 #pragma once
 
 #include "../partition/partition.h"
+#include "../insertion_sort/insertion_sort.h"
 
 #include <iterator>
 #include <functional>
 #include <utility>
 #include <cstdlib>
 
+namespace NQuickSortTraits {
+    const size_t QuadraticSortCount = 7;
+}
+
 template <class Iterator, class Comparator = std::less<decltype(*std::declval<Iterator>())>>
 void QuickSort(Iterator first, Iterator last, const Comparator& comparator = Comparator())
 {
     auto count = std::distance(first, last);
-    if (count < 2) {
+    if (count < NQuickSortTraits::QuadraticSortCount) {
+        InsertionSort(first, last, comparator);
         return;
     }
 
